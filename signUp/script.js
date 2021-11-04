5cb10d26e562905221127cadcbc443a214a76217
Maia Dinsmore <69313479+maiad37@users.noreply.github.com>
2021-11-03 23:15:50 -0300

    updated luiz

--- signUp/script.js
+++ signUp/script.js
@@ -2,7 +2,6 @@ let fpass1 = document.getElementById('fpass1')
 let fpass2 = document.getElementById('fpass2')
 let passMatch = document.getElementById('passMatch')
 
-
 function checkPass(){
     if(fpass1.value !== fpass2.value){
         passMatch.style.color = "red";
@@ -13,5 +12,21 @@ function checkPass(){
     }
 }
 
-const register = ()
+// const register = (e) =>{
+//     e.preventDefault()
 
+//     const {femail, fnumber, fpass1, fpass2} = e.target.elements
+
+//     console.log(femail.value)
+// }
+
+form.addEventListener("submit", function(e) {
+  e.preventDefault();
+  var data = new FormData(form);
+  const {fnumber} = data
+  console.log(data.fnumber.value)
+  console.log(fnumber)
+  for (const [name,value] of data) {
+    // console.log(name,value)
+  }
+});
